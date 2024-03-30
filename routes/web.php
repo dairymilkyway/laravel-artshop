405df8fad8d968d405a381944c7d472a9e958f98
Gwyn <skythekid211@gmail.com>
2024-03-30 11:08:24 +0800

    added dashboard / cartforms

--- routes/web.php
+++ routes/web.php
@@ -6,12 +6,22 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//artist index
+//customer index
 Route::get('/home', function () {
     return view('customer.index');
 });
 
+//customer cart
+Route::get('/cart', function () {
+    return view('customer.cartform');
+});
+
+
 // artist create artwork
 Route::get('/artist/create', function () {
     return view('artist.createartwork');
+});
+
+Route::get('/artist/dashboard', function () {
+    return view('artist.artistdashboard');
 });
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e96cdec457f16e175c9da9fdc58d893b2d5a09b8',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -380,9 +380,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e96cdec457f16e175c9da9fdc58d893b2d5a09b8',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
